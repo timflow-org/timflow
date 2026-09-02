7667b16753e05627cf0f25f6c03560034c120e3f
Mark Bakker <markbak@gmail.com>
2026-09-02 10:44:50 +0200

    little clean up

--- timflow/transient/stripareasink.py
+++ timflow/transient/stripareasink.py
@@ -167,7 +167,6 @@ class HstarXsection(Element):
         self.nbar = self.nbar / self.model.p
         self.nbar = self.aq.lab**2 * np.sum(self.aq.coef * self.nbar, axis=0)
         self.nbar = self.nbar[np.newaxis, :, :]
-        self.nbarold = 0.0
         self.dischargeinf = self.aq.coef[0, :] * self.flowcoef * self.resfac
         self.dischargeinflayers = np.sum(
             self.dischargeinf * self.aq.eigvec[self.layers, :, :], 1
@@ -181,7 +180,6 @@ class HstarXsection(Element):
             aq = self.model.aq.find_aquifer_data(x, y)
         rv = np.zeros((self.nparam, aq.naq, self.model.npval), dtype=complex)
         if aq == self.aq:
-            # rv[:] = self.term + self.term2 + self.nbarold
             rv[:] = self.term + self.term2 + self.nbar
         return rv
 
