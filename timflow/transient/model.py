fe268cdc2facf4903ad1bf78fe92e8d19e839977
Mark Bakker <markbak@gmail.com>
2026-09-07 20:43:23 +0200

    remove unused function

--- timflow/transient/model.py
+++ timflow/transient/model.py
@@ -453,25 +453,6 @@ class Model:
             rv += aq.headsemitoplayer(x, y, z, t)
         return rv
 
-    def headll(self, z, t, aq, returneta=False):
-        """Head in leaky layer caused by loading efficiency."""
-        z = np.atleast_1d(z)
-        t = np.atleast_1d(t)
-        rv = np.zeros((len(z), len(t)))
-        headbar = -1 / self.p
-        for iz in range(len(z)):
-            lay, _, _ = aq.findlayer(z[iz])
-            eta = (
-                headbar * np.sinh(aq.alpha[lay] * (z[iz] - aq.zaqtop[lay]))
-                + headbar * np.sinh(aq.alpha[lay] * (aq.zaqbot[lay - 1] - z[iz]))
-            ) / np.sinh(aq.alpha[lay] * aq.Hll[lay])
-            if returneta:
-                return eta
-            rv[iz] = invlapgen(
-                t, eta, self.M, self.tintervals, np.array([0.0]), np.array([1.0])
-            )
-        return rv
-
     def velocompold(self, x, y, z, t, aq=None, layer_ltype=[0, 0]):
         # implemented for one layer
         if aq is None:
